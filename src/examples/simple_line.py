b31d78c7358a70bfe3ebd8164582f043a7e3e207
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2024-03-12 16:38:20 -0700

    Create sample graph

---
+++ src/examples/simple_line.py
@@ -0,0 +1,29 @@
+from dash import Dash, html, dcc, callback, Output, Input
+import dash_bootstrap_components as dbc
+import pandas as pd
+import plotly.express as px
+
+
+app = Dash(__name__)
+
+df = pd.read_csv(
+    'https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv'
+)
+
+@callback(
+    Output('graph-content', 'figure'),
+    Input('dropdown-selection', 'value')
+)
+def update_graph(value):
+    dff = df[df.country==value]
+    return px.line(dff, x='year', y='pop')
+
+app.layout = html.Div([
+    html.H1(children='Title of Dash App', style={'textAlign':'center'}),
+    dcc.Location(id="url", refresh=False),
+    dcc.Dropdown(df.country.unique(), 'Canada', id='dropdown-selection'),
+    dcc.Graph(id='graph-content'),
+])
+
+if __name__ == '__main__':
+    app.run(debug=True)
